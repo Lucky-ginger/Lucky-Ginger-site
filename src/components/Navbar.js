1e87e4130c06a3a79f708917ab71ea02eb96d6cb
Brennan Kreiman <brennankreiman@gmail.com>
2018-08-06 16:55:25 -0500

    nav bar setup
    naming nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import Link from 'gatsby-link'
-
-import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
 
 const Navbar = () => (
@@ -10,7 +8,7 @@ const Navbar = () => (
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+            <img src={logo} alt="Lucky Ginger" style={{ width: '88px' }} />
           </figure>
         </Link>
       </div>
@@ -19,20 +17,8 @@ const Navbar = () => (
           About
         </Link>
         <Link className="navbar-item" to="/products">
-          Products
+          Menu
         </Link>
-      </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
       </div>
     </div>
   </nav>
